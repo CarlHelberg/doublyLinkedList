bf10d6c1162d7919dc51fbd5b99e04dcc38fbe7e
Carl <carlhelberg.se@gmail.com>
2019-07-25 20:59:15 +0200

    Functionality added to return LinkedList as string with ToString()

--- LinkedList.cs
+++ LinkedList.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp4
 {
-    class Node<T>
+        class Node<T>
         {
             public T value;
             public Node<T> next;
@@ -16,24 +16,68 @@ namespace ConsoleApp4
                 next = null;
             }
         }
-        static class LinkedList<T>
+         class  LinkedList<T>
         {
-            public static Node<T> Head;
+            public   Node<T> Head;
+            public   int Length = 0;
 
-            public static void Prepend( T prependValue)
+            public  void Prepend( T prependValue)
             {
                 Node<T> PrependThis = new Node<T>(prependValue);
                 PrependThis.next = Head;
                 Head = PrependThis;
-                
+                Length++;
             }
+
+            public override string ToString()
+            {
+                string toReturn = "(";
+                var tempValue = Head;
+                var tempNext = tempValue.next;
+                while(tempValue != null)
+                {
+                    tempNext = tempValue.next;
+                    toReturn += tempValue.value;
+                    if(tempNext != null)
+                    {
+                        toReturn += ", ";
+                    }
+                    tempValue = tempValue.next; 
+                }
+                toReturn += ")";
+                System.Console.WriteLine(toReturn);
+                return toReturn;
+            }
+
+            public void Traverse()
+            {
+                var tempValue = Head;
+                while(tempValue != null)
+                {
+                    
+                    System.Console.WriteLine(tempValue.value);
+                    tempValue = tempValue.next; 
+                }
+                System.Console.WriteLine(Head.value);
+            }
+            
         }
         
     class Program
     {
         static void Main(string[] args)
         {
-            
+            LinkedList<int> thisDamnList = new LinkedList<int>();
+            thisDamnList.Prepend(2);
+            thisDamnList.Prepend(456);
+            thisDamnList.Prepend(56);
+            thisDamnList.Prepend(5);
+            thisDamnList.Prepend(564);
+            thisDamnList.Prepend(34);
+            thisDamnList.Prepend(76);
+            System.Console.WriteLine("Done appending");
+            thisDamnList.ToString();
+            Console.Read();
         }
     }
 }
