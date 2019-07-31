3a711477e1a1628dbdf820cd5cb2bcc08acedbe3
Carl <carlhelberg.se@gmail.com>
2019-07-31 21:37:29 +0200

    Method added to insert a value after a specified index

--- doublyLinkedList.cs
+++ doublyLinkedList.cs
@@ -26,6 +26,41 @@ namespace doublyLinkedList
         {
             public Node<T> Head;
             public int Length = 0;
+            
+            public bool InsertAfterIndex(int _index , T _value)
+            {
+                var current = Head;
+                var tempNext = current.next;
+                if(_index >= Length)
+                {
+                    System.Console.WriteLine("The index specified does not exist.\n Please try again with InsertAfterIndex(index, value)!");
+                    return false;
+                }
+                Node <T> insertThis = new Node<T>(_value);
+                if(_index == 0)
+                {
+                    tempNext = current.next;
+                    insertThis.next = current.next;
+                    Head.next = insertThis;
+                    insertThis.previous = Head;
+                    return true;
+                }
+                current = Head.next;
+                tempNext = current.next;
+                for(int i = 1; i < Length; i++)
+                {
+                    if( i == _index)
+                    {
+                        current.next = insertThis;
+                        insertThis.next = tempNext;
+                        insertThis.previous = current;
+                        return true;
+                    }
+                    current = current.next;
+                    tempNext = current.next;
+                }
+                return false;
+            }
         }
 
         static void Main(string[] args)
