30e9909b0803fc7502cbd57a690f064592c115f4
Carl <carlhelberg.se@gmail.com>
2019-07-31 19:20:51 +0200

    Added functionality to delete a node at provided index

--- doublyLinkedList.cs
+++ doublyLinkedList.cs
@@ -37,6 +37,53 @@ namespace doublyLinkedList
         {
             public static Node<T> Head;
             public static int Length = 0;
+
+            public bool DeleteAtIndex(uint _index)
+            {
+                var current = Head;
+                var beforeDeleted = current.previous;
+                var afterDeleted = current.next;
+
+                if (_index < Length)
+                {
+                    if (_index == 0)
+                    {
+                        Head = current.next;
+                        return true;
+                    }
+                    else 
+                    {
+                        beforeDeleted = current;
+                        current = current.next;
+                        afterDeleted = current.next;
+                        for (int i = 0; i < Length; i++)
+                        {
+
+                            if (i + 1 == _index)
+                            {
+                                beforeDeleted = current;
+                                current = current.next;
+                                afterDeleted = current.next;
+                                current.next = null;
+                                current.previous = null;
+                                beforeDeleted.next = afterDeleted;
+                                afterDeleted.previous = beforeDeleted;
+                                Length--;
+                                return true;
+                            }
+                            beforeDeleted = current;
+                            current = current.next;
+                            afterDeleted = current.next;
+                        }
+                        return true;
+                    }
+                }
+                else
+                {
+                    System.Console.WriteLine("The index you are trying to delete does not exist!");
+                    return false;
+                }
+            }
         }
 
         static void Main(string[] args)
