59a7b0e78d42b531455463fdfca56430ea96f440
Carl <carlhelberg.se@gmail.com>
2019-08-02 17:04:52 +0200

    Changed skeleton on doublyLinkedList. Class Node<T> is now nested inside doublyLinkedList

--- doublyLinkedList.cs
+++ doublyLinkedList.cs
@@ -8,22 +8,22 @@ namespace doublyLinkedList
 {
     class Program
     {
-        class Node<T>
-        {
-            public object value;
-            public Node<T> next;
-            public Node<T> previous;
-
-            public Node (T _value)
-            {
-                value = _value;
-                next = null;
-                previous = null;
-            }
-        }
-
         class DoublyLinkedList<T>
         {
+            class Node<T>
+            {
+                public object value;
+                public Node<T> next;
+                public Node<T> previous;
+
+                public Node (T _value)
+                {
+                    value = _value;
+                    next = null;
+                    previous = null;
+                }
+            }
+
             public Node<T> Head;
             public int Length = 0;
         }
