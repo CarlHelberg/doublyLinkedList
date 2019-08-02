5d1b34b32cb4efb47735ab365afdfddd070696ee
Carl <carlhelberg.se@gmail.com>
2019-08-02 17:23:46 +0200

    Added functionality to Reference elements in the list by index

--- doublyLinkedList.cs
+++ doublyLinkedList.cs
@@ -27,6 +27,28 @@ namespace doublyLinkedList
             public Node<T> Head;
             public int Length = 0;
 
+            private object GetIndex(int _index)
+            {
+                var IndexReturn = Head;
+                for(int i = 0; i < Length; i++)
+                {
+                    if (i == _index)
+                    {
+                        return IndexReturn.value;
+                    }
+                    IndexReturn = IndexReturn.next;
+                }
+                throw new Exception(" The index you are looking for does not exist");
+
+            }
+            
+            public object this[int _index]
+            {
+                get
+                {
+                    return (GetIndex(_index));
+                }
+            }
         }    
         static void Main(string[] args)
         {
